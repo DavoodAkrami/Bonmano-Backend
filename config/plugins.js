99bc2662bcfbeb85e6febe8df185e823495516d2
Davood <davood.akrami.rk2025@gmail.com>
2025-06-10 17:02:01 +0330

    Fixing errors

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,23 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwt: {
+        expiresIn: '7d',
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        sizeLimit: 100000,
+      },
+    },
+  },
+  'api-token': {
+    enabled: true,
+    config: {
+      tokenExpiration: 7 * 24 * 60 * 60 * 1000, // 7 days
+    },
+  },
+});
